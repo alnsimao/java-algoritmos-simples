665c89d45e9f2405d8c80f3cdaec56d43e3e0c4b
alnsimao <alansimao9a1@gmail.com>
2025-03-07 19:00:14 -0300

    Foram feitas algumas correções e mudanças no código, adicionado novos atributos no objeto.
    Criados novos metodos para calcular a media e se o aluno ficou de exame
    ou não. Próximo passo é saber como vou calcular a nova nota baseado no
    exame.

--- curso_programacao/src/application/Aluno.java
+++ curso_programacao/src/application/Aluno.java
@@ -8,6 +8,8 @@ public class Aluno {
 	double nota1;
 	double nota2;
 	double media;
+	String situacao;
+	double notaExame;
   
 	public Aluno(String nome, String curso,int semestre,double nota1,double nota2, double media) {
 		this.nome = nome;
@@ -15,14 +17,27 @@ public class Aluno {
 		this.semestre = semestre;
 		this.nota1 = nota1;
 		this.nota2 = nota2;
-		this.media = media;
+		this.media = calcularMedia();
 		
 	}
 	
 	public double calcularMedia() {
-		media = (nota1 + nota2)/ 2;
-		return media;
+		return (nota1 + nota2)/ 2;
+			
+		}
 	
-		
+	public void situaçãoAluno() {
+		if (this.media < 3 ) {
+			this.situacao = "Reprovado";
+		} else if (this.media >=3 && this.media <=5) {
+			this.situacao = "Exame";
+			
+			} else {
+				this.situacao = "Aprovado";
+			}
+			 
+
+			
+		}
 	}
-}
+	
