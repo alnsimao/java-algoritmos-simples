8f1b7be222e2cb380936e058639b063cafbcf8ba
alnsimao <alansimao9a1@gmail.com>
2025-03-07 19:17:09 -0300

    Removi os construtores pois percebi que não é necessário já que o usuário que vai inserir os dados.

--- curso_programacao/src/Cadastro_Alunos.java
+++ curso_programacao/src/Cadastro_Alunos.java
@@ -1,11 +1,17 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+import application.Aluno;
+
 public class Cadastro_Alunos {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		Aluno aluno = new Aluno();
+		
+		System.out.println("Insira o nome do aluno");
 		
 		
 		
--- curso_programacao/src/application/Aluno.java
+++ curso_programacao/src/application/Aluno.java
@@ -1,7 +1,7 @@
 package application;
 
 public class Aluno {
-	
+
 	String nome;
 	String curso;
 	int semestre;
@@ -10,34 +10,30 @@ public class Aluno {
 	double media;
 	String situacao;
 	double notaExame;
-  
-	public Aluno(String nome, String curso,int semestre,double nota1,double nota2, double media) {
-		this.nome = nome;
-		this.curso = curso;
-		this.semestre = semestre;
-		this.nota1 = nota1;
-		this.nota2 = nota2;
-		this.media = calcularMedia();
-		
-	}
-	
-	public double calcularMedia() {
-		return (nota1 + nota2)/ 2;
-			
-		}
-	
-	public void situaçãoAluno() {
-		if (this.media < 3 ) {
-			this.situacao = "Reprovado";
-		} else if (this.media >=3 && this.media <=5) {
-			this.situacao = "Exame";
-			
-			} else {
-				this.situacao = "Aprovado";
-			}
-			 
 
-			
+	public double calcularMedia() {
+		return (nota1 + nota2) / 2;
+
+	}
+
+	public void situaçãoAluno() {
+		if (this.media < 3) {
+			this.situacao = "Reprovado";
+		} else if (this.media >= 3 && this.media <= 5) {
+			this.situacao = "Exame";
+
+		} else {
+			this.situacao = "Aprovado";
+		} 
+		
+	} public void exameAluno(double notaExame) {
+		this.situacao.equals("Exame");
+		this.notaExame = notaExame;
+		
+		if(this.notaExame > 6) {
+			this.situacao = "Aprovado";
+		} else {
+			this.situacao = "Reprovado";
 		}
 	}
-	
+}
